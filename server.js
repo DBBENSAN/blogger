042e7f5484a10f89521cab872d9b9f973977c485
DBENSAN <dftb97@gmail.com>
2023-02-08 22:00:25 -0800

    add server and util files

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const path = require('path');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
-
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
+
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
+  secret: 'insertKeyHereSmile',
+  cookie: {
+    maxAge: 5000,
+    httpOnly: true,
+    secure: true,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize,
+
   }),
 };
 
---
+++ utils/helpers.js
@@ -0,0 +1,11 @@
+module.exports = {
+  format_date: (date) => {
+
+    return date.toLocaleDateString();
+  },
+  format_amount: (amount) => {
+    // format large numbers with commas
+    return parseInt(amount).toLocaleString();
+  }
+
+};
